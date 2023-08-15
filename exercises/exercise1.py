40250ad3705d7c512941c7602450715b9555fa44
marioajn <maritojimenez2003@outlook.com>
2023-08-15 18:33:52 -0300

    primer ejercicio resuelto

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -6,11 +6,12 @@ Calcular el área del cuadrado usando las variables disponibles.
 Restricción: Usar el operador de multiplicación
 """
 
-lado_cuadrado = 5
+lado_cuadrado = 5*5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado
 # COMPLETAR - FIN
+print(lado_cuadrado)
 
 assert area_cuadrado == 25
 
@@ -19,12 +20,12 @@ assert area_cuadrado == 25
 Re-Escribir usando el operador de potencia.
 """
 
-lado_cuadrado = 5
+lado_cuadrado = 5**2
 
 # COMPLETAR - INICIO
-
+print(lado_cuadrado)
 # COMPLETAR - FIN
-
+area_cuadrado = lado_cuadrado
 assert area_cuadrado == 25
 
 
@@ -32,12 +33,12 @@ assert area_cuadrado == 25
 Re-Escribir usando la función pow.
 """
 
-lado_cuadrado = 5
-
+lado_cuadrado = pow( 5, 2)
+print(lado_cuadrado)
 # COMPLETAR - INICIO
 
 # COMPLETAR - FIN
-
+area_cuadrado = lado_cuadrado
 assert area_cuadrado == 25
 
 
@@ -49,8 +50,9 @@ Restricción: Usar el operador de división entera.
 precio = 3.74
 presupuesto_disponible = 10
 
+cantidad_a_comprar =int(presupuesto_disponible/precio)
 # COMPLETAR - INICIO
-
+print(cantidad_a_comprar)
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -62,9 +64,9 @@ Restricción: Usar el operador módulo.
 """
 
 numero_incalculable = 2 ** 54 - 1
-
+es_divisible_por_siete=numero_incalculable%7
 # COMPLETAR - INICIO
-
+print(es_divisible_por_siete)
 # COMPLETAR - FIN
 
-assert es_divisible_por_siete
+assert es_divisible_por_siete==0
